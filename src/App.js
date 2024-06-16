9ee17116b450fc867c77ae79ec21128bb9f9e873
M L <lakshmidurga2009@gmail.com>
2024-06-16 17:40:29 -0400

    NewCollections_NewsLetter_Offers

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import Navbar from'./Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import LoginSignup from './Pages/LoginSignUp';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div>  
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      
+      <Footer/>
+      
+      </BrowserRouter>    
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+
+const Shop = () => {
+  return (
+    <div>    
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
